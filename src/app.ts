a01a4cbc78bbcab2a9e997c68daa41acaf85eb5b
douglaswenzel <douglas.wenzel@hotmail.com>
2025-12-14 11:40:34 -0300

    feat(app.ts): configuração inicial da aplicação

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+// src/app.ts
+
+import express, { Application } from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+const app: Application = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api', routes); 
+
+app.get('/', (req, res) => {
+    res.status(200).send({ message: 'API Emballer está rodando! 📦' });
+});
+
+export default app;
