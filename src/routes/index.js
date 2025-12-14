05d1811a2a36f02439da169195dcf2c7d707e6a0
douglaswenzel <douglas.wenzel@hotmail.com>
2025-12-13 22:35:39 -0300

    feat(Routes): Inserida rotas de salas e andares na inicialização de rots.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const andarRoutes = require('./andar.routes');
+const salaRoutes = require('./sala.routes');
+
+router.use('/andares', andarRoutes);
+router.use('/salas', salaRoutes); 
 
 router.get('/', (req, res) => {
     res.status(200).send({ message: 'Acesso às rotas API OK.' });
